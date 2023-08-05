4b5c02c57b5588e8351cc292e36ecfe87859a81d
ohilikeit <yjh091500@naver.com>
2023-08-05 22:39:32 +0900

    chore: add requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,12 @@
+pip install pandas
+pip install geopandas
+pip install pydeck
+pip install mip
+pip install matplotlib
+pip install shapely
+
+
+# 한글 matplotlib 시각화를 위한 폰트 설치 
+sudo apt-get install -y fonts-nanum
+sudo fc-cache -fv
+rm ~/.cache/matplotlib -rf
